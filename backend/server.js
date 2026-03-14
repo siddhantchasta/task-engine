d7328142e43664fae7f8326334f85e1f489bd838
Siddhant Chasta <siddhantchasta4199@gmail.com>
2026-03-14 17:04:05 +0530

    Fixed Port Issue

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDB Connected"))
 .catch(err=>console.log(err))
 
-const PORT = 5001
+const PORT = process.env.PORT || 5001
 app.listen(PORT, ()=>{
  console.log("Server running on port 5001")
 })
